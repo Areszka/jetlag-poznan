7ec8dc9e285666a7448edb3836af373b912b11de
Agnieszka Grabowska <aneszka0301@gmail.com>
2025-02-19 15:45:04 +0100

    chore: add vitest library

---
+++ __tests__/login.test.tsx
@@ -0,0 +1,24 @@
+import { expect, test, vi } from "vitest";
+import { render, screen } from "@testing-library/react";
+import Page from "@/app/(auth)/login/page";
+
+vi.mock("next/navigation", async () => {
+  const actual = await vi.importActual("next/navigation");
+  return {
+    ...actual,
+    useRouter: vi.fn(() => ({
+      push: vi.fn(),
+      replace: vi.fn(),
+    })),
+    useSearchParams: vi.fn(() => ({
+      get: vi.fn(),
+    })),
+    usePathname: vi.fn(),
+  };
+});
+
+test("Render Login Page", () => {
+  render(<Page />);
+  expect(screen.getByRole("heading", { level: 2, name: "Login" })).toBeDefined();
+  expect(screen.getByRole("button", { name: "Login" })).toBeDefined();
+});
